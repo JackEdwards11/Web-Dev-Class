87cbb0affbc5cdd02297536345f68bd5e84d9466
JackEdwards11 <jackmedwards@gmail.com>
2023-02-20 13:10:17 -0500

    4.1

--- form.js
+++ form.js
@@ -1,8 +1,13 @@
+setTimeout(cookie, 200);
+
+function cookie(){
+    alert("We use cookies and related technology to deliver relevant advertising on our site, in emails, and across the Internet. We and our partners also use these technologies to personalize content and perform site analytics.");
+}
 var modal = document.getElementById("myModal");
 
 var button = document.getElementById("sub");
 
 button.onclick = function(event){
     event.preventDefault();
-    modal.style.display = "block";
+    modal.style.display = "inline";
 }
